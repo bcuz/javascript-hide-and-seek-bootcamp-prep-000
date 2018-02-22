62803cfde99f64a4094d017828c6c3b6d47b84d7
adam N <adam.nawroc@gmail.com>
2018-02-22 12:19:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  var nested = document.getElementById('nested')
+  return nested.getElementsByClassName('target')[0]
+}
+
+function increaseRankBy(n) {
+  const lis = document.querySelectorAll('ul.ranked-list li')
+
+  for (let i = 0; i < lis.length; i++) {
+  lis[i].innerHTML = parseInt(lis[i].innerHTML) + n
+}
+
+}
+
+function deepestChild() {
+  var ele = document.getElementById('grand-node')
+
+  while (ele.querySelector('div')) {
+    var ele = ele.querySelector('div')
+  }
+
+  return ele
+}
